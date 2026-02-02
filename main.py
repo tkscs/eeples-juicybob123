d8781c90dea4d67e77ba88d4b301bbe1dd3a44f3
juicybob123 <okruh2028@kehillahstudent.org>
2026-02-02 09:14:47 -0800

    i forgot to comit this

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+
+
+song = "I like to eat, eat, eat, apples and bananas"
+
+
+
+# def sing(vowel):
+#     new_scentence = ""
+#     for charachter in song:
+#       new_scentence = new_scentence * 3
+#     print(new_scentence)
+
+
+
+# sing("ay")
+# sing("ee")
+# sing("i")
+# sing("o")
+# sing("u")
+
+
 song = "I like to eat, eat, eat, apples and bananas"
 
 def sing(vowel):
-    # your code here
+    print(song[:20].replace("a", vowel)+ song[20:])
+    
+
 
 sing("ay")
 sing("ee")
